f73213b3428fe4edf8fd03a80dfcec91b26fc405
CharricosFront2 <142949252+CharricosFront2@users.noreply.github.com>
2025-02-24 15:19:25 -0600

    Correcciones

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements AfterViewInit {
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngAfterViewInit(): void {
+    this.route.fragment.subscribe((fragment: any) => {
+      if (fragment) {
+        const element = document.getElementById(fragment);
+        if (element) {
+          // Obtener la posición del elemento y agregar un margen de desplazamiento
+          const topOffset = 60; // Ajusta el valor según el espacio que desees
+          const elementPosition = element.getBoundingClientRect().top;
+          const offsetPosition =
+            elementPosition + window.pageYOffset - topOffset;
+
+          window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth',
+          });
+        }
+      }
+    });
+  }
+}
